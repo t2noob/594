108921af1b80c9873f15ee7c3afbae2f77905c41
t2noob <t2noob@gmail.com>
2013-01-17 14:06:30 -0800

    Update experiments/hw1.js

--- experiments/hw1.js
+++ experiments/hw1.js
@@ -3,6 +3,6 @@ var var1;
 var1="First time Variable is declared";
 document.write(var1);
 document.write("<br>");
-firstname="Second time variable is declared.";
+var1="Second time variable is declared.";
 document.write(var1);
 </script> 
