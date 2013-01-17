57db27e2e1d385278bb6ebe458e4ceb0b9a576be
t2noob <t2noob@gmail.com>
2013-01-17 14:05:51 -0800

    update script to contain something.

---
+++ experiments/hw1.js
@@ -0,0 +1,8 @@
+<script>
+var var1;
+var1="First time Variable is declared";
+document.write(var1);
+document.write("<br>");
+firstname="Second time variable is declared.";
+document.write(var1);
+</script> 
